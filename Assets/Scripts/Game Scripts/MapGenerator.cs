5fc907d2657e9bf2b769aad60f4dfc0f1703fe91
MichaelGusak-cmd <mmll4444@gmail.com>
2022-11-08 02:51:30 -0600

    ruletime/tilemap setup

--- Assets/Scripts/Game Scripts/MapGenerator.cs
+++ Assets/Scripts/Game Scripts/MapGenerator.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    private int ugh = 3;
-    private int grrr = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,4 +15,42 @@ public class MapGenerator : MonoBehaviour
     {
         
     }
+    /*
+    public static void RenderMap(int[,] map, Tilemap tilemap, TileBase tile)
+    {
+        //Clear the map (ensures we dont overlap)
+        tilemap.ClearAllTiles();
+        //Loop through the width of the map
+        for (int x = 0; x < map.GetUpperBound(0) ; x++)
+        {
+            //Loop through the height of the map
+            for (int y = 0; y < map.GetUpperBound(1); y++)
+            {
+                // 1 = tile, 0 = no tile
+                if (map[x, y] == 1)
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                }
+            }
+        }
+    }
+    public static int[,] GenerateArray(int width, int height, bool empty)
+    {
+        int[,] map = new int[width, height];
+        for (int x = 0; x < map.GetUpperBound(0); x++)
+        {
+            for (int y = 0; y < map.GetUpperBound(1); y++)
+            {
+                if (empty)
+                {
+                    map[x, y] = 0;
+                }
+                else
+                {
+                    map[x, y] = 1;
+                }
+            }
+        }
+        return map;
+    }*/
 }
